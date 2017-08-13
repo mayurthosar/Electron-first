7cf63275932e2a2604314e7c1e259447106a1e7e
Mayur Thosar <mayurthosar.mt@gmail.com>
2017-08-14 04:14:22 +0530

    second commit

--- home.js
+++ home.js
@@ -1,11 +1,27 @@
-const remote = require('electron').remote;
-const main = remote.require('./app.js');
+// const remote = require('electron').remote;
+// const main = remote.require('./app.js');
+//
+// var button = document.createElement('button')
+// button.textContent='Open Window';
+// button.addEventListener('click',() => {
+//     var window=remote.getCurrentWindow();
+//     main.openWindow('about');
+//     window.close();
+//     },false)
+// document.body.appendChild(button)
 
-var button = document.createElement('button')
-button.textContent='Open Window';
-button.addEventListener('click',() => {
-    var window=remote.getCurrentWindow();
-    main.openWindow('about');
-    window.close();
-    },false)
-document.body.appendChild(button)
+//Angular code
+//=================================================================
+
+var app = angular.module('first-app',['ngRoute']);
+
+app.config(function($routeProvider){
+
+    $routeProvider
+        .when('/',{
+        templateUrl:"about.html"
+        })
+        .when('/about',{
+            templateUrl:'about.html'
+        });
+})
